8c68040cc98a3b8eaf144b62376a1875233bca02
Ben Knighton <131706686+BenKnighton@users.noreply.github.com>
2023-06-13 10:09:59 +0100

    Initial commit

---
+++ Gender.py
@@ -0,0 +1,47 @@
+import librosa
+import numpy as np
+import keras
+
+gender_model  = keras.models.load_model("gender_model.h5")
+
+
+#Gender Recognition
+
+def extract_gender_feature(file_name, **kwargs):
+    mfcc = kwargs.get("mfcc")
+    chroma = kwargs.get("chroma")
+    mel = kwargs.get("mel")
+    contrast = kwargs.get("contrast")
+    tonnetz = kwargs.get("tonnetz")
+    # print(mfcc, chroma, mel, contrast, tonnetz)
+    X, sample_rate = librosa.core.load(file_name)
+    if chroma or contrast:
+        stft = np.abs(librosa.stft(X))
+    result = np.array([])
+    if mfcc:
+        mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+        result = np.hstack((result, mfccs))
+    if chroma:
+        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+        result = np.hstack((result, chroma))
+    if mel:
+        mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+        result = np.hstack((result, mel))
+    if contrast:
+        contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+        result = np.hstack((result, contrast))
+    if tonnetz:
+        tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+        result = np.hstack((result, tonnetz))
+    return result
+
+def predict_gender(audio_path):
+    features = extract_gender_feature(audio_path, mel=True).reshape(1, -1)
+    male_prob = gender_model.predict(features)[0][0]    
+    # print(male_prob)  
+    if male_prob > (1 - male_prob):
+        return "Male"
+    else:
+        return "Female"
+
+print(predict_gender("combined.wav"))
---
+++ README.md
@@ -0,0 +1,2 @@
+# Gender-Identification
+ A.I to Differentiate gender detected on an audio file (using Machine Learning)
